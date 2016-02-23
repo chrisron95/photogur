187c0e5ade3572d775318c952fcff3f54cc8158e
chrisron95 <chris@chrisheder.com>
2016-02-23 17:35:51 -0500

    Replaced image URL option with image FILE option.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,4 @@
 class Picture < ActiveRecord::Base
+  has_attached_file :file, :styles => { :medium => "500x500>", :thumb => "100x100>" }#, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
 end
